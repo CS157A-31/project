ede1d991e829e7970816ef59be96cba37384f9ed
Christian Castro <ccastro715@gmail.com>
2019-11-08 20:38:30 -0800

    Integrated server API call

--- client/src/actions/login/loginApiCall.js
+++ client/src/actions/login/loginApiCall.js
@@ -16,20 +16,12 @@ export const attemptLogin = userData => dispatch => {
   axios
     .post(`${HOST}${LOGIN_URI}`, userData)
     .then(res => {
-<<<<<<< HEAD
       // Set userToken to Local Storage
       setTokenToLocalStorage(USER_ID, res.data.userID);
-
-=======
       console.log('attempt login callAPI');
       console.log(res.data);
-<<<<<<< HEAD
->>>>>>> Initial Login page setup
       const payload = dispatch(attemptLoginSuccessfully(res.data));
       return payload;
-=======
-      dispatch(attemptLoginSuccessfully(res.data));
->>>>>>> Initial Login page setup
     })
     .catch(err => {
       console.log('error');
--- client/src/page/LoginPage.js
+++ client/src/page/LoginPage.js
@@ -3,23 +3,11 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Redirect, withRouter } from 'react-router-dom';
 
-<<<<<<< HEAD
 import { attemptLogin } from '../actions/login/loginApiCall';
-=======
-<<<<<<< HEAD
-import { attemptLogin } from "../actions/login/loginApiCall";
->>>>>>> Initial Login page setup
 
 import Login from '../components/auth/Login.js';
 
 class LoginPage extends Component {
-=======
-//import { attemptRegister } from "../actions/register/registerApiCall";
-
-import Login from "../components/auth/Login.js";
-
-class RegisterPage extends Component {
->>>>>>> Initial Login page setup
   constructor() {
     super();
     this.state = {
@@ -38,17 +26,10 @@ class RegisterPage extends Component {
   onClick = e => {
     e.preventDefault();
 
-<<<<<<< HEAD
     this.props.attemptLogin({
       email: this.state.email,
       password: this.state.password
     });
-=======
-    // this.props.attemptRegister({
-    //   email: this.state.email,
-    //   password: this.state.password
-    // });
->>>>>>> Initial Login page setup
   };
 
   render() {
@@ -59,20 +40,8 @@ class RegisterPage extends Component {
     const { userID } = user;
     console.log('User is: ' + user);
 
-<<<<<<< HEAD
     return user.email && userID ? (
       <Redirect to='/home' />
-=======
-    return user.email ? (
-<<<<<<< HEAD
-      <Redirect to="/" />
-=======
-      <div className="container">
-        <h1>User Login</h1>
-        Email: {user.email}
-      </div>
->>>>>>> Initial Login page setup
->>>>>>> Initial Login page setup
     ) : (
       <Login
         onChange={e => this.onChange(e)}
@@ -93,18 +62,11 @@ function mapStateToProps(state) {
 }
 
 function matchDispatchToProps(dispatch) {
-<<<<<<< HEAD
   return bindActionCreators({ attemptLogin }, dispatch);
-=======
-  return bindActionCreators({ attemptRegister }, dispatch);
->>>>>>> Initial Login page setup
 }
 
 export default connect(
   mapStateToProps,
   matchDispatchToProps
-<<<<<<< HEAD
 )(withRouter(LoginPage));
-=======
-)(withRouter(RegisterPage));
->>>>>>> Initial Login page setup
+
