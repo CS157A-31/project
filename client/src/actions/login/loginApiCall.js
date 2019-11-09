0c0cd0d8a072eee7da7170294a59d8a9e41be7ea
Christian Castro <ccastro715@gmail.com>
2019-11-08 18:31:08 -0800

    Initial Login page setup

---
+++ client/src/actions/login/loginApiCall.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+import {
+  attemptRegisterSuccessfully,
+  attemptRegisterFailed
+} from './attemptLoginAction';
+
+import { HOST, LOGIN_URI } from '../../constant';
+
+// Register
+export const attemptLogin = userData => dispatch => {
+  axios
+    .post(`${HOST}${LOGIN_URI}`, userData)
+    .then(res => {
+      console.log('attempt login callAPI');
+      console.log(res.data);
+      dispatch(attemptLoginSuccessfully(res.data));
+    })
+    .catch(err => {
+      console.log('error');
+      console.log(err);
+      dispatch(attemptLoginFailed(err));
+    });
+};
