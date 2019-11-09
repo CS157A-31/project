8373ca1aa6555f04f51a8ee79750ba1f026a93bc
Christian Castro <ccastro715@gmail.com>
2019-11-08 18:31:08 -0800

    Initial Login page setup

--- client/src/page/LoginPage.js
+++ client/src/page/LoginPage.js
@@ -3,11 +3,23 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Redirect, withRouter } from 'react-router-dom';
 
+<<<<<<< HEAD
 import { attemptLogin } from '../actions/login/loginApiCall';
+=======
+<<<<<<< HEAD
+import { attemptLogin } from "../actions/login/loginApiCall";
+>>>>>>> Initial Login page setup
 
 import Login from '../components/auth/Login.js';
 
 class LoginPage extends Component {
+=======
+//import { attemptRegister } from "../actions/register/registerApiCall";
+
+import Login from "../components/auth/Login.js";
+
+class RegisterPage extends Component {
+>>>>>>> Initial Login page setup
   constructor() {
     super();
     this.state = {
@@ -26,10 +38,17 @@ class LoginPage extends Component {
   onClick = e => {
     e.preventDefault();
 
+<<<<<<< HEAD
     this.props.attemptLogin({
       email: this.state.email,
       password: this.state.password
     });
+=======
+    // this.props.attemptRegister({
+    //   email: this.state.email,
+    //   password: this.state.password
+    // });
+>>>>>>> Initial Login page setup
   };
 
   render() {
@@ -40,8 +59,20 @@ class LoginPage extends Component {
     const { userID } = user;
     console.log('User is: ' + user);
 
+<<<<<<< HEAD
     return user.email && userID ? (
       <Redirect to='/home' />
+=======
+    return user.email ? (
+<<<<<<< HEAD
+      <Redirect to="/" />
+=======
+      <div className="container">
+        <h1>User Login</h1>
+        Email: {user.email}
+      </div>
+>>>>>>> Initial Login page setup
+>>>>>>> Initial Login page setup
     ) : (
       <Login
         onChange={e => this.onChange(e)}
@@ -62,10 +93,18 @@ function mapStateToProps(state) {
 }
 
 function matchDispatchToProps(dispatch) {
+<<<<<<< HEAD
   return bindActionCreators({ attemptLogin }, dispatch);
+=======
+  return bindActionCreators({ attemptRegister }, dispatch);
+>>>>>>> Initial Login page setup
 }
 
 export default connect(
   mapStateToProps,
   matchDispatchToProps
+<<<<<<< HEAD
 )(withRouter(LoginPage));
+=======
+)(withRouter(RegisterPage));
+>>>>>>> Initial Login page setup
